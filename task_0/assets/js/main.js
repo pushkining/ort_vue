e0ec01507cebf94c9576bc8678bc70de87770f2d
pushkining <pushkining@ua.fm>
2021-03-18 22:44:13 +0200

    v2.6

--- task_0/assets/js/main.js
+++ task_0/assets/js/main.js
@@ -1,19 +1,8 @@
 let arr1 = [3, 6, 7, 9];
-let arr2 = [56, 78, 34];
+let arr2 = [56, 78, 34, 7, 89, -52, 1, 6, 45, 99, 3, -5, 0];
 
-function sad() {
-    return false;
-}
 
-function das() {
-    console.log('yyyyyyyyyy');
-}
+let tmp = arr2.slice(0); // чтобы не изменять порядок элементов в исходном массиве, если это критично
+tmp.sort(new Function('x, y', 'return x - y')); // если не критично, то вместо tmp пропишите mas
 
-arr2.__proto__ = das;
-arr1.__proto__ = sad;
-
-console.log(arr1);
-console.log(arr2);
-
-let a = sad();
-console.log(a);
+console.log(tmp.slice(0, 2));
--- task_6_1/assets/js/main.js
+++ task_6_1/assets/js/main.js
@@ -1,16 +1,7 @@
 (async function () {
     let apiPB = [];
-    // async function getPosition() {
-    //     navigator.geolocation.getCurrentPosition(position => {
-    //         geo = position.coords 
-    //         console.log(geo);
-    //      },error => {
-    //          console.log(error);
-    //      })   
-    // }
-    for (let y = 0; y > 9; y++) {
-        console.log(y);
-    }
+
+
     function getCoords() {
         /* Создаём и возвращаем Promise, это позволит при вызове функции применить await */
         return new Promise((resolve, reject) => {
@@ -31,19 +22,33 @@
     let url = "https://filebase.xyz/pbatm/";
     apiPB = await fetch(url);
     apiPB = await apiPB.json();
-    
+
     console.log(apiPB.devices)
     // }
-
+    let apiPBTemp = [];
     async function evclidGeometry() {
-        for await(let i of apiPB.devices) {
-            let distance = Math.sqrt((+apiPB.devices[i].latitude - userCoords.lat) ** 2 + (+apiPB.devices[i].longitude - userCoords.lon) ** 2);
-            console.log(`Расстояние до банкомата: ${distance}`);
+        let rad = Math.PI / 180;
+        let r = 6371;
+        for (let i = 0; i <= apiPB.devices.length; i++) {
+            //let distance = Math.sqrt((+apiPB.devices[i].latitude*Math.PI/180 - userCoords.lat*Math.PI/180) ** 2 + (+apiPB.devices[i].longitude*Math.PI/180 - userCoords.lon*Math.PI/180) ** 2);
+            let distance = 2 * r * Math.asin(Math.sqrt(Math.sin((+apiPB.devices[i].latitude * rad - userCoords.lat * rad) / 2)**2 + Math.cos(userCoords.lat * rad) * Math.cos(+apiPB.devices[i].latitude * rad) * Math.sin((+apiPB.devices[i].longitude * rad - userCoords.lon * rad) / 2) ** 2))
+            apiPB.devices[i].distance = distance;
+            //apiPBTemp.push(distance);
+            //console.log(`Расстояние до банкомата: ${distance}`);
         }
     }
-    let dist = await evclidGeometry();
-    console.log(dist);
-    let distance = Math.sqrt((+apiPB.devices[0].latitude - userCoords.lat) ** 2 + (+apiPB.devices[0].longitude - userCoords.lon) ** 2)
-    console.log("Finish", distance);
+    function getMinDistance() {
+        console.log(Math.min(...apiPBTemp.devices.distance))
+        // apiPBTemp = apiPB.devices.slice(0);
+        // apiPBTemp.sort(new Function('x,y', 'return x-y'));
+        // console.log(apiPBTemp.slice(0, 5))
+    }
+    evclidGeometry();
+    getMinDistance();
+    console.log(apiPB);
+
+    //let distance = Math.sqrt((+apiPB.devices[0].latitude - userCoords.lat) ** 2 + (+apiPB.devices[0].longitude - userCoords.lon) ** 2)
+    console.log("Finish");
 
 })()
+
