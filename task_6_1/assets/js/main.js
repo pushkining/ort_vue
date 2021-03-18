66df0c5235c2e9d8b42c8d4311100d2db61770b0
pushkining <pushkining@ua.fm>
2021-03-18 19:32:10 +0200

    v2.5

--- task_6_1/assets/js/main.js
+++ task_6_1/assets/js/main.js
@@ -1,22 +1,49 @@
+(async function () {
+    let apiPB = [];
+    // async function getPosition() {
+    //     navigator.geolocation.getCurrentPosition(position => {
+    //         geo = position.coords 
+    //         console.log(geo);
+    //      },error => {
+    //          console.log(error);
+    //      })   
+    // }
+    for (let y = 0; y > 9; y++) {
+        console.log(y);
+    }
+    function getCoords() {
+        /* Создаём и возвращаем Promise, это позволит при вызове функции применить await */
+        return new Promise((resolve, reject) => {
 
-let geo = new Object;
+            globalThis.navigator.geolocation.getCurrentPosition(successInfo => {
+                resolve({
+                    lat: successInfo.coords.latitude,
+                    lon: successInfo.coords.longitude
+                },
+                );
+            }, failInfo => reject(failInfo))
+        });
+    }
+    let userCoords = await getCoords();
+    console.log(userCoords);
 
-(async function(){
-let url ="https://filebase.xyz/pbatm/";
-setTimeout(async function(){
-    let apiPB = await fetch(url);
-    apiPB = await apiPB.json();},2000)
-    navigator.geolocation.getCurrentPosition(position => {
-       geo = position.coords
+    //async function getATM() {
+    let url = "https://filebase.xyz/pbatm/";
+    apiPB = await fetch(url);
+    apiPB = await apiPB.json();
+    
+    console.log(apiPB.devices)
+    // }
 
-    },error => {
-        console.log(error);
-    })
-    console.log(apiPB);
-    console.log(geo); 
+    async function evclidGeometry() {
+        for await(let i of apiPB.devices) {
+            let distance = Math.sqrt((+apiPB.devices[i].latitude - userCoords.lat) ** 2 + (+apiPB.devices[i].longitude - userCoords.lon) ** 2);
+            console.log(`Расстояние до банкомата: ${distance}`);
+        }
+    }
+    let dist = await evclidGeometry();
+    console.log(dist);
+    let distance = Math.sqrt((+apiPB.devices[0].latitude - userCoords.lat) ** 2 + (+apiPB.devices[0].longitude - userCoords.lon) ** 2)
+    console.log("Finish", distance);
+
 })()
-
-
-function evclidGeometry(latitude, longitude){
-
-}
