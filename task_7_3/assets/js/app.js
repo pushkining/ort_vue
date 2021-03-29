8d7836b10b25d8007e14e70cfba1dd8d25be95d3
pushkining <pushkining3@gmail.com>
2021-03-29 14:23:38 +0300

    v3.0

--- task_7_3/assets/js/app.js
+++ task_7_3/assets/js/app.js
@@ -1,25 +1,20 @@
-
-// const doAjax = async () => {
-//     const response = await fetch('./assets/api.php'); // Генерируем объект Response
-//     if (response.ok) {
-//         console.log(response);
-//         const jVal = await response.json(); // Парсим тело ответа
-//         return Promise.resolve(jVal);
-//         }
-//     else
-//         return Promise.reject('*** PHP file not found');
-//     }
-
-// doAjax().then(console.log).catch(console.log); 
-export default class GetService {
-    _apiBase = 'http://localhost:5500';
-    url = "/assets/ap[i.json"
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}` +
-                `, received ${res.status}`);
+const itemName = document.querySelector('.item-name');
+const itemCountry = document.querySelector('.item-country');
+const itemScore = document.querySelector('.item-score');
+const doAjax = async () => {
+    const response = await fetch('./assets/api.json'); // Генерируем объект Response
+    if (response.ok) {
+        const jVal = await response.json(); // Парсим тело ответа
+        return Promise.resolve(jVal);
         }
-        return await res.json();
+    else
+        return Promise.reject('*** File not found');
     }
+
+doAjax().then((data)=>{
+for(const item of data) {
+    itemName.innerHTML += `${item.name}<br>`;
+    itemCountry.innerHTML += `${item.country}<br>`;
+    itemScore.innerHTML += `${item.score}<br>`;
 }
+}).catch(console.log);
--- task_7_3/assets/js/main.js
+++ task_7_3/assets/js/main.js
@@ -1,11 +1,3 @@
-// let arr1 = [3, 6, 7, 9];
-// let arr2 = [56, 78, 34, 7, 89, -52, 1, 6, 45, 99, 3, -5, 0];
-
-
-// let tmp = arr2.slice(0); // чтобы не изменять порядок элементов в исходном массиве, если это критично
-// tmp.sort(new Function('x, y', 'return x - y')); // если не критично, то вместо tmp пропишите mas
-
-// console.log(tmp.slice(0, 2));
 
 const settings = {
 	"async": true,
