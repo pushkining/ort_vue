ac8aedb873a979dc2bf089bd4fcf24b972d9ce36
pushkining <pushkining3@gmail.com>
2021-03-29 17:47:06 +0300

    v3.1

--- task_7_3/assets/js/main.js
+++ task_7_3/assets/js/main.js
@@ -7,9 +7,8 @@ const settings = {
 
 $.ajax(settings).done((data) => {
 	for (const itera of data) {
-		// console.log(itera.name,itera.country)
-		$('.item-name').append(` ${itera.name}<br>`);
-		$('.item-country').append(` ${itera.country}<br>`);
-		$('.item-score').append(` ${itera.score}<br>`);
+		$('.item-name').append(`${itera.name}<br>`);
+		$('.item-country').append(`${itera.country}<br>`);
+		$('.item-score').append(`${itera.score}<br>`);
 	}
 });
